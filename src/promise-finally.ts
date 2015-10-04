5d8c0561f1fd93d6b9e7690fdf74ea5cd0bd7cf6
Blake Embrey <hello@blakeembrey.com>
2015-10-03 18:00:26 -0700

    Initial commit

---
+++ src/promise-finally.ts
@@ -0,0 +1,7 @@
+export default function promiseFinally (value: any, cb: Function) {
+  return Promise.resolve(value)
+    .then(
+      value => Promise.resolve(cb()).then(() => value),
+      reason => Promise.resolve(cb()).then(() => Promise.reject(reason))
+    )
+}
